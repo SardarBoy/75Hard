b6f2c4aae798845a41bd85c6150f490ff2a38d64
Gurtej singh <45142106+SardarBoy@users.noreply.github.com>
2025-10-23 17:12:46 +0100

    Add files via upload

---
+++ leetcode/python/groupanagrams.py
@@ -0,0 +1,31 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dic=defaultdict(list)
+        for s in strs:
+            alphabets=[0]*26
+            for c in s:
+                alphabets[ord(c)-ord('a')]+=1
+            dic[tuple(alphabets)].append(s)
+            print(alphabets)
+        return list(dic.values())
+            
+# Notes:
+# Used count of each alphabet to check anagrams.
+# List can’t be key in dict, so change to tuple.
+# Same letter count = same group.
+
+""" When you use a key, Python computes a hash value (a fixed number) for that key. This hash value tells Python where to look. so we havw to convert list to tuple"""
+
+""" [1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
+[1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
+[1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
+[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
+"""
+
+"""
+[[[1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0],["eat","tea","ate"]],
+[[1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0],["tan","nat"]],
+[[1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0],["bat"]]]
+"""
